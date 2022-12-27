0ef57a29c45a6112ad5bfd996ed8cfd03fe33117
RayhanalKavey <rayhanalkavey@gmail.com>
2022-12-27 16:22:41 +0600

    Add darkmood system for both onClick and user preference when the page render initially. Install react icon and react router.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { RiSwitchFill } from "react-icons/ri";
+// import { FaBeer } from "@react-icons/all-files/fa/FaBeer";
 
 function App() {
-  return <div className="App">hi</div>;
+  const [theme, setTheme] = useState("");
+  // Check the user's system preference (1st render of the page)
+  useEffect(() => {
+    if (window.matchMedia("(prefers-color-scheme:dark)").matches) {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  }, []);
+
+  // Check whether user select dark or light coming from  handleThemeSwitch
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+  // Theme Switch handler
+  const handleThemeSwitch = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+  return (
+    <div className="h-screen bg-white dark:bg-slate-900 flex justify-center items-center">
+      <h1 className="text-3xl font-bold dark:text-fuchsia-100 ">Hello World</h1>
+      <button
+        className="bg-green-200 dark:bg-black p-4 rounded-3xl"
+        onClick={handleThemeSwitch}
+      >
+        <RiSwitchFill color={`${theme === "dark" ? "white" : "black"}`} />
+      </button>
+    </div>
+  );
 }
 
 export default App;
