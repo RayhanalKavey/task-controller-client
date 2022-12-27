c6645470fd599fd4da1e630474d0a4b8532c9d5f
RayhanalKavey <rayhanalkavey@gmail.com>
2022-12-28 05:41:25 +0600

    Complete auth provider. Created a small hook named useAuth, to use the component any where in the application without exporting.

--- src/CONTEXT/AuthProvider/AuthProvider.js
+++ src/CONTEXT/AuthProvider/AuthProvider.js
@@ -1,6 +1,15 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 import app from "../../FIREBASE/firebase.config";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+  GoogleAuthProvider,
+  signInWithPopup,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
 
 const AUTH_CONTEXT = createContext();
 const auth = getAuth(app);
@@ -9,6 +18,7 @@ const AuthProvider = ({ children }) => {
   const [user, setUser] = useState("");
   ///leading state to prevent the reload log out issue
   const [loading, setLoading] = useState(true);
+  const [reload, setReload] = useState(true);
 
   //Sign Up
   const createUser = (email, password) => {
@@ -16,13 +26,50 @@ const AuthProvider = ({ children }) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
   // SignIn With email and password
+  const login = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
   // Update user profile (updateProfile)
+  const updateUserProfile = (profile) => {
+    setLoading(true);
+    return updateProfile(auth.currentUser, profile);
+  };
   // Google sign in
-  // On auth state change. Observe user
-  // Sign-out
+  const googleProvider = new GoogleAuthProvider();
+  const googleLogin = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  //On auth state change. Observe user
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      console.log("Inside auth state change", currentUser);
+      setUser(currentUser);
+      setLoading(false);
+    });
+    return () => unsubscribe();
+  }, [reload]);
+
+  //Sign-out
+  const logout = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
   // Verify email
 
-  const authInfo = {};
+  const authInfo = {
+    user,
+    setUser,
+    createUser,
+    login,
+    updateUserProfile,
+    googleLogin,
+    logout,
+    loading,
+    setReload,
+  };
   return (
     <AUTH_CONTEXT.Provider value={authInfo}>{children}</AUTH_CONTEXT.Provider>
   );
